d7fb6cf526b25efc2975e3b687c05dec45933e5d
cristian <pcris>
2021-12-16 03:15:52 +0000

    Update

--- src/database.js
+++ src/database.js
@@ -3,7 +3,8 @@ const mongoose= require('mongoose');
 const URI=process.env.MONGODB_URI ? process.env.MONGODB_URI:'mongodb://localhost/test2';
 mongoose.connect(URI,{
     useNewUrlParser:true,
-    useCreateIndex:true
+    useCreateIndex:true,
+    useUnifiedTopology: true
 });
 
 const connection=mongoose.connection ;
